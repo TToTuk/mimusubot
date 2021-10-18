91c5f161659c3d37b57a08aef1ae77a63e6a5939
TToTuk <63599437+TToTuk@users.noreply.github.com>
2021-10-18 21:01:50 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+TOKEN = 'token'
+
+ST1 = 'CAACAgIAAxkBAAEDCoZhYFykKHJtmKCAEjJBOeJW7IQHJwAC4Q8AAss9AUv-CaqlPgL6oSEE'
+ST2 = 'CAACAgIAAxkBAAEDCoxhYFzOauYt12bjNKmKGoZ-rUuEXgACUQ4AApjjCEtTk0SGKKggSiEE'
+ST3 = 'CAACAgIAAxkBAAEDCpNhYFz5KNgfiNPcaf-iddDa5XFx2QACEg8AAhFMAUs6j8FK8_iZKSEE'
+
+butStart = 'Показать список команд'
+
+help_text = '''Список команд:
+/help - Помощь (список команд)
+/rnum "Число" - Случайное число (по умолчанию хначение "число" = 100)
+/coin - Подкинуть монетку
+/wiki "Предмет поиска" - Поиск по Википедии
+
+Переводчик:
+/ru - С английского на русский
+/en - С русского на английский
+'''
